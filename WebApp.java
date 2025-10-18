36c8473d63a025858bc6cf628161aacd09d44639
Srilaxmi1747 <srilaxmi.veeramalla@gmail.com>
2025-10-18 11:30:34 +0530

    Fixed duplicate variable and removed old file

--- WebApp.java
+++ WebApp.java
@@ -6,8 +6,8 @@ public class WebApp {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", exchange -> {
-            String response = "<h1>Hello, Student Project Running Successfully!</h1>";
             String response = "<h1>Hello, Student Project Running Successfully 2nd time!</h1>";
+            
             exchange.sendResponseHeaders(200, response.getBytes().length);
             exchange.getResponseBody().write(response.getBytes());
             exchange.getResponseBody().close();
